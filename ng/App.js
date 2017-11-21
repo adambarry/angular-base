0ddee73befdf53563f847a83aff23e8c8da29161
Adam Barry <admbarry@gmail.com>
2017-11-21 22:02:06 +0100

    chore: housekeeping

--- ng/App.js
+++ ng/App.js
@@ -1,15 +1,15 @@
-(function () {
+(function() {
     "use strict";
-    
+
     var id = "App";
-    
+
     var App = angular.module(id, [
         //Reference/list main modules
         "ui.router",
         "restangular",
         "jcs.angular-http-batch",
         "pascalprecht.translate"
-    ]).run(["$rootScope", "$state", function ($rootScope, $state) {
+    ]).run(["$rootScope", "$state", function($rootScope, $state) {
         /**
          * Force $state to run by injecting it. Without this, $state only runs
          * when a controller or something else asks for it via
